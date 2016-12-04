a43d37a0e65d52e39ba6a7b052bd7585347afdd1
n_gavi01 <n_gavi01@uni-muenster.de>
2016-12-04 19:43:28 +0100

    Image Update

--- GoogleScholar/content.js
+++ GoogleScholar/content.js
@@ -5,9 +5,10 @@ chrome.runtime.onMessage.addListener( /*Listen to JSON response from background.
   function(request, sender, sendResponse) {
     if( request.message === "clicked_browser_action" ) {
     	$( "#gs_qsuggest" ).removeClass( "gs_ri" ); /*To remove badge from suggested result div */
+      $('<img id="peer-review" src='+chrome.extension.getURL("peer-review.png")+' />' ).insertAfter( ".gs_ri" ); //add badge
+      
     	$('.gs_rt').each(function(i, obj) {
-
-    	if (obj.childNodes[0].text != undefined) {
+      if (obj.childNodes[0].text != undefined) { // Evaluation 
    				console.log(obj.childNodes[0].text); //Title of research to be passed to API to check if peer reviewed/reproducible
    			} else  { 
    				console.log(obj.childNodes[2].text); //Title of research to be passed to API to check if peer reviewed/reproducible
@@ -16,8 +17,7 @@ chrome.runtime.onMessage.addListener( /*Listen to JSON response from background.
     	//console.log(obj);
 
     	}); 
-        //$(".gs_ri").css("border","3px solid red"); /*Useful just to show which DIVS it selects */
-        //$('<img id="peer-review" src='+chrome.extension.getURL("peer-review.png")+' />' ).insertAfter( ".gs_ri" ); 
+        //$(".gs_ri").css("border","3px solid red"); /*Useful just to show which DIVS it selects *
     }
   }
 );
