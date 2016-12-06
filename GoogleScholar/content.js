bf105a0abe84786a9fb4201279029d40e966c200
n_gavi01 <n_gavi01@uni-muenster.de>
2016-12-06 18:20:15 +0100

    Badge fix

--- GoogleScholar/content.js
+++ GoogleScholar/content.js
@@ -6,16 +6,13 @@ chrome.runtime.onMessage.addListener( /*Listen to JSON response from background.
     if( request.message === "clicked_browser_action" ) {
     	$( "#gs_qsuggest" ).removeClass( "gs_ri" ); /*To remove badge from suggested result div */
       $('<img id="peer-review" src='+chrome.extension.getURL("peer-review.png")+' />' ).insertAfter( ".gs_ri" ); //add badge
-      
+
     	$('.gs_rt').each(function(i, obj) {
       if (obj.childNodes[0].text != undefined) { // Evaluation 
    				console.log(obj.childNodes[0].text); //Title of research to be passed to API to check if peer reviewed/reproducible
    			} else  { 
    				console.log(obj.childNodes[2].text); //Title of research to be passed to API to check if peer reviewed/reproducible
    			}
-
-    	//console.log(obj);
-
     	}); 
         //$(".gs_ri").css("border","3px solid red"); /*Useful just to show which DIVS it selects *
     }
