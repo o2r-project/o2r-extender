2788bfea6d0d19a752e1b275b50bb0e93904a405
n_gavi01 <n_gavi01@uni-muenster.de>
2016-12-07 15:16:15 +0100

    Integrating Extensions

---
+++ O2R Badge Integrator/PLOScontent.js
@@ -0,0 +1,17 @@
+// content.js
+alert("Welcome to PLOS! Press the O2R icon to assess which of these researches are executable!")
+
+chrome.runtime.onMessage.addListener( /*Listen to JSON response from background.js */
+  function(request, sender, sendResponse) {
+    if( request.message === "clicked_browser_action" ) {
+      //$('<img id="Executability Status" src='+chrome.extension.getURL("Executability-Yes-brightgreen.png")+' />').css('display', 'inline-block').insertAfter( ".gs-title>a" ); // add peer review badge
+      $('<img id="Executability Status" src='+chrome.extension.getURL("executability.png")+' />').css('margin-left', '20px').insertAfter( ".gs-title>a" ); // add peer review badge
+      $('.gs-title>a').each(function(i, obj) {
+      if (i%2!=0) {      
+      console.log(obj.text);
+    	}
+      }); 
+        //$(".gs_ri").css("border","3px solid red"); /*Useful just to show which DIVS it selects *
+    }
+  }
+);
---
+++ O2R Badge Integrator/content.js
@@ -0,0 +1,20 @@
+// content.js
+alert("Welcome to Google Scholar! Press the O2R icon to assess which of these researches are Peer-Reviewed!")
+
+chrome.runtime.onMessage.addListener( /*Listen to JSON response from background.js */
+  function(request, sender, sendResponse) {
+    if( request.message === "clicked_browser_action" ) {
+    	$( "#gs_qsuggest" ).removeClass( "gs_ri" ); /*To remove badge from suggested result div */
+      $('<img id="peer-review" src='+chrome.extension.getURL("peer-review.png")+' />' ).insertAfter( ".gs_ri" ); //add badge
+
+    	$('.gs_rt').each(function(i, obj) {
+      if (obj.childNodes[0].text != undefined) { // Evaluation 
+   				console.log(obj.childNodes[0].text); //Title of research to be passed to API to check if peer reviewed/reproducible
+   			} else  { 
+   				console.log(obj.childNodes[2].text); //Title of research to be passed to API to check if peer reviewed/reproducible
+   			}
+    	}); 
+        //$(".gs_ri").css("border","3px solid red"); /*Useful just to show which DIVS it selects *
+    }
+  }
+);
