911c35147eaad16d6aea5fa1ee1b39c455957188
n_gavi01 <n_gavi01@uni-muenster.de>
2016-12-13 19:04:57 +0100

    Create Functions & run on refresh

--- O2R Badge Integrator/GScontent.js
+++ O2R Badge Integrator/GScontent.js
@@ -1,21 +1,55 @@
 // content.js
-alert("Welcome to Google Scholar! Press the O2R icon to assess which of these researches are Peer-Reviewed!")
+var title = "";
+alert("Welcome to Google Scholar! Please wait while these search results are being assessed for peer-review & executability...")
 
 chrome.runtime.onMessage.addListener( /*Listen to JSON response from background.js */
   function(request, sender, sendResponse) {
     if( request.message === "clicked_browser_action" ) {
-    	$( "#gs_qsuggest" ).removeClass( "gs_ri" ); /*To remove badge from suggested result div */
-      $('<img id="peer-review" src='+chrome.extension.getURL("peer-review.png")+' />' ).insertAfter( ".gs_ri" ); //add badge
-
-    	$('.gs_rt').each(function(i, obj) {
-      if (obj.childNodes[0].text != undefined) { // Evaluation 
-   				var title= (obj.childNodes[0].text); //Create title of research to be passsed to CrossRefs API
-          
-   			} else  { 
-   				var title= (obj.childNodes[2].text); //Create title of research to be passsed to CrossRefs API
+    $( "#gs_qsuggest" ).removeClass( "gs_ri" ); /*To remove badge from 'Suggested Result' div */
+      //$('<img id="peer-review" src='+chrome.extension.getURL("peer-review.png")+' />' ).insertAfter( ".gs_ri" ); //add badges after every result
+    $('.gs_rt').each(function(i, obj) {
+    getTitle(obj);
+      if (title != "NONE") { //If NOT a citation, but an actual link to research, run the DOI check
+   			if (getDOI(title)!="No Match") { //If research has a doi, add badge
+   				$('<img id="peer-review" src='+chrome.extension.getURL("peer-review.png")+' />' ).insertAfter(obj);
+   				if (checkExe(title)==true) {
+   				$('<img id="peer-review" src='+chrome.extension.getURL("executability.png")+' />' ).insertAfter(obj);
+   				}
+   			} else { //If research doesn't match a doi, print to console name of research
+   				console.log("No result found for "+title); //No valid title found, input NONE string
    			}
+   		} else {
+   			console.log (" Found citation, not a research link");
+   		}         
+    	}); 
+    }
 
-      console.log(title); // Print Research Title
+/*
+* Future APIs will go here (Executability, Reproducibility). Peer Review can be integrated in getDOI function
+*/ 
+
+function checkExe(title) {
+	return true;
+}
+
+/*
+* Extract research title from each search result
+*/
+function getTitle (obj) {
+	if (obj.childNodes[0].text != undefined)  {
+		title= (obj.childNodes[0].text); // Take 1st argument in title (In case it's e.g. Environmental quality...)
+	} else if (obj.childNodes[2].text!=undefined) {
+		title= (obj.childNodes[2].text);  //Take 3rd argument (In case it's e.g. [CITATION] Air Quality...)
+	} else {
+		title= "NONE"; //It's a citation, not a link to a research
+	} return title;
+}
+
+/*
+* Get DOI from each research title using Crossrefs API
+*/
+
+function getDOI(title) {
 
       var json = (function () { //Send research title to get DOI 
       var json = null;
@@ -29,16 +63,20 @@ chrome.runtime.onMessage.addListener( /*Listen to JSON response from background.
         }
       });
       return json;
-      }) (); 
+      }) ();
 
-      if (title.toLowerCase()==json.message.items[0].title.toString().toLowerCase()) { 
-          console.log("SUCCESS!: "+title+json.message.items[0].DOI); //title matches EXACTLY
+      if (title.toLowerCase()==json.message.items[0].title.toString().toLowerCase()) {  //title matches EXACTLY to Crossrefs first search result
+          return (json.message.items[0].DOI); //return DOI of research from search results
       }
-      else { 
-          console.log("No exact match");
+      else { //title doesn't match EXACTLY, rejected 
+          return ("No Match");
+     } 
       }
 
-    	}); 
-    }
+     /* FOR TESTING ONLY
+      console.log("TITLE:"+title.toLowerCase()+"END"); // Print Research Title
+      console.log("RESULT:"+json.message.items[0].title.toString().toLowerCase()+"END"); 
+      */   
+
   }
 );
--- O2R Badge Integrator/PLOScontent.js
+++ O2R Badge Integrator/PLOScontent.js
@@ -1,28 +1,44 @@
 // content.js
-alert("Welcome to PLOS! Press the O2R icon to assess which of these researches are executable!")
+var doi;
+alert("Welcome to PLOS! Please wait while these search results are being assessed for peer-review & executability...")
 
 chrome.runtime.onMessage.addListener( /*Listen to JSON response from background.js */
   function(request, sender, sendResponse) {
     if( request.message === "clicked_browser_action" ) {
-      //$('<img id="Executability Status" src='+chrome.extension.getURL("Executability-Yes-brightgreen.png")+' />').css('display', 'inline-block').insertAfter( ".gs-title>a" ); // add peer review badge
-      $('<img id="Executability Status" src='+chrome.extension.getURL("executability.png")+' />').css('margin-left', '20px').insertAfter( ".gs-title>a" ); // add peer review badge
-      $('.gs-title>a').each(function(i, obj) {
-      if (i%2!=0) {
-        var link = obj.href;
-        var index = obj.href.indexOf("id")+3;
-        var PDFindex = obj.indexOf("PDF");
+      //$('<img id="Executability Status" src='+chrome.extension.getURL("Executability-Yes-brightgreen.png")+' />').css('display', 'inline-block').insertAfter( ".gs-title>a" ); // add badge after each result with styling
+      //$('<img id="Executability Status" src='+chrome.extension.getURL("executability.png")+' />').css('margin-left', '20px').insertAfter( ".gs-title>a" ); // add badge after each result with styling
+    $('.gs-title>a').each(function(i, obj) {
+      if (i%2!=0) { /*Return every second result, otherwise there is double duplicate results */
 
-        if (index==-1) { //no id in result
-          console.log("no doi found");
-        } else if (PDFindex ==-1) { //no pdf in result
-          console.log(obj.href.substr(index));
-        } else { //regular result
-          console.log(obj.href.substr(index,obj.href.length-4));
-        }
+      /* Extract DOI From link URL*/
+      var link = obj.href;
+      var index = obj.href.indexOf("id")+3;
+      var PDFindex = obj.href.indexOf("PDF");
 
+      getDOI(link, index, PDFindex);
+      
+      if (doi!="NONE") { //A valid DOI
+        console.log(doi)
+        $('<img id="peer-review" src='+chrome.extension.getURL("peer-review.png")+' />' ).insertAfter(obj)
+      } else { // no DOI found
+        console.log("no DOI found for "+obj.childNodes[0].textContent+" "+obj.childNodes[1].textContent);
+      }
+     
     	}
       }); 
-        //$(".gs_ri").css("border","3px solid red"); /*Useful just to show which DIVS it selects *
     }
+
+    function getDOI (link, index, PDFindex) {
+    if (index==-1) { //no id in result
+        doi="NONE";
+      } else if (PDFindex ==-1) { //no pdf in result
+        doi=(link.substr(index));
+      } else { //regular result
+        doi=(link.substr(index,link.length-4));
+        } 
+
+    }
+
+
   }
 );
