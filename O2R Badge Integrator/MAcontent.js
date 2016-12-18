1610b9443b850d7f8742da3c991265e7aabe9f5b
m_goet09 <m_goet09@uni-muenster.de>
2016-12-18 16:29:29 +0100

    update

--- O2R Badge Integrator/MAcontent.js
+++ O2R Badge Integrator/MAcontent.js
@@ -1,29 +1,53 @@
 // content.js
-alert("Welcome to Microsoft Academic Search! Press the O2R icon to assess which of these researches have a Source link!")
-//var firstHref = $("a[href^='http']").eq(0).attr("href"); /*Return first link */
-//console.log(firstHref); /*Write first link of page in console log */
+var title = "";
+alert("Welcome to Micrsoft Academic Search! Please wait while these search results are being assessed for peer-review & executability...")
 
 chrome.runtime.onMessage.addListener( /*Listen to JSON response from background.js */
   function(request, sender, sendResponse) {
     if( request.message === "clicked_browser_action" ) {
-    	//Need to insert the evaluation (by checking if a source link is available) here! 
-    if( $('.actions').length ) {
-       	$('<img id="source" src='+chrome.extension.getURL("source.png")+' />' ).insertAfter( ".actions" );
-    }
-    if( $('.entity-news card').length ) {
-      	$('<img id="source" src='+chrome.extension.getURL("nosource.png")+' />' ).insertAfter( ".entity-news card" );
-    }
-
     
-    	$('.blue-title').each(function(i, obj) {
-      if (document.getElementsByClassName("blue-title").title != undefined) { // Evaluation 
-   				var title= (document.getElementsByClassName("blue-title").title); //Create title of research to be passsed to CrossRefs API
-          
-   			} else  { 
-   				var title= (document.getElementsByClassName("blue-title").title); //Create title of research to be passsed to CrossRefs API
+    	$('.story-title').each(function(i, obj) {
+    var title = document.getElementsByClassName('story-title').getElementsByTagName('a')[0].innerHTML;
+    console.log(title);
+    return title;
+   			if (getDOI(title)!="No Match") { //If research has a doi, add badge
+   				$('<img id="peer-review" src='+chrome.extension.getURL("peer-review.png")+' />' ).insertAfter(obj);
+   				if (checkExe(title)==true) {
+   				$('<img id="peer-review" src='+chrome.extension.getURL("executability.png")+' />' ).insertAfter(obj);
+   				}
+   			} else { //If research doesn't match a doi, print to console name of research
+   				console.log("No result found for "+title); //No valid title found, input NONE string
    			}
+   		});
 
-      console.log(title); // Print Research Title
+      	$('#blue-title').each(function(i, obj) {
+    var title = document.getElementsByClassName("blue-title").title;
+    return title;
+    console.log(title);
+   			if (getDOI(title)!="No Match") { //If research has a doi, add badge
+   				$('<img id="peer-review" src='+chrome.extension.getURL("peer-review.png")+' />' ).insertAfter(obj);
+   				if (checkExe(title)==true) {
+   				$('<img id="peer-review" src='+chrome.extension.getURL("executability.png")+' />' ).insertAfter(obj);
+   				}
+   			} else { //If research doesn't match a doi, print to console name of research
+   				console.log("No result found for "+title); //No valid title found, input NONE string
+   			}
+   		}); 
+    }
+
+/*
+* Future APIs will go here (Executability, Reproducibility). Peer Review can be integrated in getDOI function
+*/ 
+
+function checkExe(title) {
+	return true;
+}
+
+/*
+* Get DOI from each research title using Crossrefs API
+*/
+
+function getDOI(title) {
 
       var json = (function () { //Send research title to get DOI 
       var json = null;
@@ -37,18 +61,22 @@ chrome.runtime.onMessage.addListener( /*Listen to JSON response from background.
         }
       });
       return json;
-      }) (); 
+      }) ();
 
-      if (title.toLowerCase()==json.message.items[0].title.toString().toLowerCase()) { 
-          console.log("SUCCESS!: "+title+json.message.items[0].DOI); //title matches EXACTLY
+      if (title.toLowerCase()==json.message.items[0].title.toString().toLowerCase()) {  //title matches EXACTLY to Crossrefs first search result
+          return (json.message.items[0].DOI); //return DOI of research from search results
       }
-      else { 
-          console.log("No exact match");
+      else { //title doesn't match EXACTLY, rejected 
+          return ("No Match");
+     } 
       }
 
-    	}); 
+     /* FOR TESTING ONLY
+      console.log("TITLE:"+title.toLowerCase()+"END"); // Print Research Title
+      console.log("RESULT:"+json.message.items[0].title.toString().toLowerCase()+"END"); 
+      */   
 
-    }
   }
 );
+
 
