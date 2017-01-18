70b48b31f7d947a36beba7d4909edf860cfb4213
Antonia <antoniaicq@web.de>
2017-01-18 11:22:30 +0100

    Add persistent settings for each badge type in the extensions options.

--- O2R Badge Integrator/options.js
+++ O2R Badge Integrator/options.js
@@ -1,30 +1,35 @@
 function save_options() {
-  var color = document.getElementById('color').value;
-  var likesColor = document.getElementById('like').checked;
-  chrome.storage.sync.set({
-    favoriteColor: color,
-    likesColor: likesColor
-  }, function() {
-    // Update status to let user know options were saved.
-    var status = document.getElementById('status');
-    status.textContent = 'Options saved.';
-    setTimeout(function() {
-      status.textContent = '';
-    }, 750);
-  });
+    var executable = document.getElementById('executable').checked;
+    var peerreview = document.getElementById('peerreview').checked;
+    var licence = document.getElementById('licence').checked;
+
+    chrome.storage.sync.set({
+        executableBadge: executable,
+        peerreviewBadge: peerreview,
+        licenceBadge: licence
+    }, function () {
+        // Update status to let user know options were saved.
+        var status = document.getElementById('status');
+        status.textContent = 'Options saved.';
+        setTimeout(function () {
+            status.textContent = '';
+        }, 750);
+    });
 }
 
 // Restores select box and checkbox state using the preferences
 // stored in chrome.storage.
 function restore_options() {
-  // Use default value color = 'red' and likesColor = true.
-  chrome.storage.sync.get({
-    favoriteColor: 'red',
-    likesColor: true
-  }, function(items) {
-    document.getElementById('color').value = items.favoriteColor;
-    document.getElementById('like').checked = items.likesColor;
-  });
+    // Use default value color = 'red' and likesColor = true.
+    chrome.storage.sync.get({
+        executableBadge: true,
+        peerreviewBadge: true,
+        licenceBadge: true
+    }, function (items) {
+        document.getElementById('executable').checked = items.executableBadge;
+        document.getElementById('peerreview').checked = items.peerreviewBadge;
+        document.getElementById('licence').checked = items.licenceBadge;
+    });
 }
 document.addEventListener('DOMContentLoaded', restore_options);
 document.getElementById('save').addEventListener('click',
