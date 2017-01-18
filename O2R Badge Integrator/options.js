a6766522edb5003d572de8088136bf0d23782f61
Antonia <antoniaicq@web.de>
2017-01-18 13:37:33 +0100

    option for transparency

--- O2R Badge Integrator/options.js
+++ O2R Badge Integrator/options.js
@@ -2,11 +2,13 @@ function save_options() {
 	var executable = document.getElementById('executable').checked;
 	var peerreview = document.getElementById('peerreview').checked;
 	var licence = document.getElementById('licence').checked;
+	var transparent = document.getElementById('transparent').checked;
 
 	chrome.storage.sync.set({
 		executableBadge: executable,
 		peerreviewBadge: peerreview,
-		licenceBadge: licence
+		licenceBadge: licence,
+        transparentArticles: transparent
 	}, function () {
 		// Update status to let user know options were saved.
 		var status = document.getElementById('status');
@@ -24,11 +26,13 @@ function restore_options() {
 	chrome.storage.sync.get({
 		executableBadge: true,
 		peerreviewBadge: true,
-		licenceBadge: true
+		licenceBadge: true,
+        transparentArticles: true
 	}, function (items) {
 		document.getElementById('executable').checked = items.executableBadge;
 		document.getElementById('peerreview').checked = items.peerreviewBadge;
 		document.getElementById('licence').checked = items.licenceBadge;
+		document.getElementById('transparent').checked = items.transparentArticles;
 	});
 }
 document.addEventListener('DOMContentLoaded', restore_options);
