cba66d670bc97b1bd35276d72969542ab2bee18a
Matthias Mohr <m_mohr08@uni-muenster.de>
2017-01-18 12:13:47 +0100

    Code formatting, removed old code (version control?), css opacity for not used entries, async loading (muuuuch faster)...

--- O2R Badge Integrator/MEcontent.js
+++ O2R Badge Integrator/MEcontent.js
@@ -1,50 +1,48 @@
 // content.js
 alert("Welcome to Mendeley! Press the O2R icon to assess which of these researches are Peer-Reviewed!")
 
-chrome.runtime.onMessage.addListener( /*Listen to JSON response from background.js */
-  function(request, sender, sendResponse) {
-    if( request.message === "clicked_browser_action" ) {
-    	if($( ".number" ).value == 0) {
-      	$('<img id="peer-review" src='+chrome.extension.getURL("noreaders.png")+' />' ).insertAfter( ".publication-details" );
-			} 
-			else{
-				$('<img id="peer-review" src='+chrome.extension.getURL("readers.png")+' />' ).insertAfter( ".publication-details" );
+chrome.runtime.onMessage.addListener(/*Listen to JSON response from background.js */
+		function (request, sender, sendResponse) {
+			if (request.message === "clicked_browser_action") {
+				if ($(".number").value == 0) {
+					$('<img id="peer-review" src=' + chrome.extension.getURL("noreaders.png") + ' />').insertAfter(".publication-details");
+				} else {
+					$('<img id="peer-review" src=' + chrome.extension.getURL("readers.png") + ' />').insertAfter(".publication-details");
+				}
+				//add badge
+
+				$('#publication-details').each(function (i, obj) {
+					if (obj.$('#publication-details').closest('a').attr('href').text != undefined) { // Evaluation 
+						var title = (obj.$('#publication-details').closest('a').attr('href').text); //Create title of research to be passed to CrossRefs API
+
+					} else {
+						var title = (obj.$('#publication-details').closest('a').attr('href').text); //Create title of research to be passed to CrossRefs API
+					}
+
+					console.log(title); // Print Research Title
+
+					var json = (function () { //Send research title to get DOI 
+						var json = null;
+						$.ajax({
+							'async': false,
+							'global': false,
+							'url': "https://api.crossref.org/works?query=" + title,
+							'dataType': "json",
+							'success': function (data) {
+								json = data;
+							}
+						});
+						return json;
+					})();
+
+					if (title.toLowerCase() == json.message.items[0].title.toString().toLowerCase()) {
+						console.log("SUCCESS!: " + title + json.message.items[0].DOI); //title matches EXACTLY
+					} else {
+						console.log("No exact match");
+					}
+
+				});
+
 			}
-			//add badge
-
-    		$('#publication-details').each(function(i, obj) {
-      if (obj.$('#publication-details').closest('a').attr('href').text != undefined) { // Evaluation 
-   				var title= (obj.$('#publication-details').closest('a').attr('href').text); //Create title of research to be passed to CrossRefs API
-          
-   			} else  { 
-   				var title= (obj.$('#publication-details').closest('a').attr('href').text); //Create title of research to be passed to CrossRefs API
-   			}
-
-      console.log(title); // Print Research Title
-
-      var json = (function () { //Send research title to get DOI 
-      var json = null;
-      $.ajax({
-        'async': false,
-        'global': false,
-        'url': "https://api.crossref.org/works?query="+title,
-        'dataType': "json",
-        'success': function (data) {
-            json = data;
-        }
-      });
-      return json;
-      }) (); 
-
-      if (title.toLowerCase()==json.message.items[0].title.toString().toLowerCase()) { 
-          console.log("SUCCESS!: "+title+json.message.items[0].DOI); //title matches EXACTLY
-      }
-      else { 
-          console.log("No exact match");
-      }
-
-    	}); 
-        
-    }
-  }
+		}
 );
--- O2R Badge Integrator/options.js
+++ O2R Badge Integrator/options.js
@@ -1,36 +1,36 @@
 function save_options() {
-    var executable = document.getElementById('executable').checked;
-    var peerreview = document.getElementById('peerreview').checked;
-    var licence = document.getElementById('licence').checked;
+	var executable = document.getElementById('executable').checked;
+	var peerreview = document.getElementById('peerreview').checked;
+	var licence = document.getElementById('licence').checked;
 
-    chrome.storage.sync.set({
-        executableBadge: executable,
-        peerreviewBadge: peerreview,
-        licenceBadge: licence
-    }, function () {
-        // Update status to let user know options were saved.
-        var status = document.getElementById('status');
-        status.textContent = 'Options saved.';
-        setTimeout(function () {
-            status.textContent = '';
-        }, 750);
-    });
+	chrome.storage.sync.set({
+		executableBadge: executable,
+		peerreviewBadge: peerreview,
+		licenceBadge: licence
+	}, function () {
+		// Update status to let user know options were saved.
+		var status = document.getElementById('status');
+		status.textContent = 'Options saved.';
+		setTimeout(function () {
+			status.textContent = '';
+		}, 750);
+	});
 }
 
 // Restores select box and checkbox state using the preferences
 // stored in chrome.storage.
 function restore_options() {
-    // Use default value color = 'red' and likesColor = true.
-    chrome.storage.sync.get({
-        executableBadge: true,
-        peerreviewBadge: true,
-        licenceBadge: true
-    }, function (items) {
-        document.getElementById('executable').checked = items.executableBadge;
-        document.getElementById('peerreview').checked = items.peerreviewBadge;
-        document.getElementById('licence').checked = items.licenceBadge;
-    });
+	// Use default value color = 'red' and likesColor = true.
+	chrome.storage.sync.get({
+		executableBadge: true,
+		peerreviewBadge: true,
+		licenceBadge: true
+	}, function (items) {
+		document.getElementById('executable').checked = items.executableBadge;
+		document.getElementById('peerreview').checked = items.peerreviewBadge;
+		document.getElementById('licence').checked = items.licenceBadge;
+	});
 }
 document.addEventListener('DOMContentLoaded', restore_options);
 document.getElementById('save').addEventListener('click',
-    save_options);
+		save_options);
