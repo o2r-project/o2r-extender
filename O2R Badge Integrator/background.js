5e46b809769525062451214e393f38494df725d4
Matthias Mohr <m_mohr08@uni-muenster.de>
2017-01-24 22:22:24 +0100

    Added filtering etc.

--- O2R Badge Integrator/background.js
+++ O2R Badge Integrator/background.js
@@ -5,3 +5,9 @@ chrome.webNavigation.onDOMContentLoaded.addListener(function (tab) {
 chrome.webNavigation.onCompleted.addListener(function (tab) {
 	chrome.tabs.sendMessage(tab.tabId, {"message": "everything_ready"});
 });
+
+chrome.extension.onMessage.addListener(function(message, sender, sendResponse){
+	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+	  chrome.tabs.sendMessage(tabs[0].id, message);
+	});
+ });
