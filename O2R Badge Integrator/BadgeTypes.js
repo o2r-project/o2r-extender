967f1855f4011b5252ecf7ee24bc100217e22260
Matthias Mohr <m_mohr08@uni-muenster.de>
2017-02-07 13:29:31 +0100

    Added small badges to detail view, too.

--- O2R Badge Integrator/BadgeTypes.js
+++ O2R Badge Integrator/BadgeTypes.js
@@ -3,12 +3,8 @@ var ExtendedView = false;
 var BadgeTypes = [
 	{
 		key: 'executable', value: 'Executable',
-		apiPath: 'executable/o2r/doi:', doiEncoded: true, extended: true,
+		apiPath: 'executable/o2r/doi:', doiEncoded: true, extended: false,
 		filter: {type: 'select', values: ['yes', 'running', 'no']}
-	},{
-		key: 'licence', value: 'Licence',
-		apiPath: 'licence/o2r/doi:', doiEncoded: true, extended: true,
-		filter: {type: 'select', values: ['open', 'mostly open', 'partially open', 'closed']}
 	},{
 		key: 'peerreview', value: 'Peer review',
 		apiPath: 'peerreview/doaj/doi:', doiEncoded: false, extended: false,
@@ -17,9 +13,13 @@ var BadgeTypes = [
 		key: 'spatial', value: 'Research location',
 		apiPath: 'spatial/o2r/doi:', doiEncoded: true, extended: false,
 		filter: {type: 'text'}
-	}/*,{
+	},{
+		key: 'licence', value: 'Licence',
+		apiPath: 'licence/o2r/doi:', doiEncoded: true, extended: true,
+		filter: {type: 'select', values: ['open', 'mostly open', 'partially open', 'closed']}
+	},/*{
 		key: 'releasetime', value: 'Release time',
-		apiPath: 'releasetime/o2r/doi:', doiEncoded: true, extended: false,
+		apiPath: 'releasetime/o2r/doi:', doiEncoded: true, extended: true,
 		filter: {type: 'text'}
 	}*/
 ];
