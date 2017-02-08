b2f6fbd81af6a5a04c62a4133ec354318853b64a
Matthias Mohr <m_mohr08@uni-muenster.de>
2017-02-08 13:11:20 +0100

    Updated badge types

--- O2R Badge Integrator/BadgeTypes.js
+++ O2R Badge Integrator/BadgeTypes.js
@@ -1,25 +1,24 @@
 var ExtendedView = false;
 // TODO: When adding a new badge, apply this here
-var BadgeTypes = [
-	{
-		key: 'executable', value: 'Executable',
-		apiPath: 'executable/o2r/doi:', doiEncoded: true, extended: false,
-		filter: {type: 'select', values: ['yes', 'running', 'no']}
-	},{
-		key: 'peerreview', value: 'Peer review',
-		apiPath: 'peerreview/doaj/doi:', doiEncoded: false, extended: false,
-		filter: {type: 'text'}
-	},{
-		key: 'spatial', value: 'Research location',
-		apiPath: 'spatial/o2r/doi:', doiEncoded: true, extended: false,
+var BadgeTypes = [{
+		key: 'releasetime', value: 'Release time',
+		apiPath: 'releasetime/crossref/doi:', doiEncoded: true, extended: false,
 		filter: {type: 'text'}
 	},{
 		key: 'licence', value: 'Licence',
 		apiPath: 'licence/o2r/doi:', doiEncoded: true, extended: true,
 		filter: {type: 'select', values: ['open', 'mostly open', 'partially open', 'closed']}
 	},{
-		key: 'release-date', value: 'Release date',
-		apiPath: 'release-date/crossref/doi:', doiEncoded: true, extended: false,
+		key: 'executable', value: 'Executable',
+		apiPath: 'executable/o2r/doi:', doiEncoded: true, extended: true,
+		filter: {type: 'select', values: ['yes', 'running', 'no']}
+	},{
+		key: 'spatial', value: 'Research location',
+		apiPath: 'spatial/o2r/doi:', doiEncoded: true, extended: false,
+		filter: {type: 'text'}
+	},{
+		key: 'peerreview', value: 'Peer review',
+		apiPath: 'peerreview/doaj/doi:', doiEncoded: false, extended: false,
 		filter: {type: 'text'}
 	}
 ];
