514b81aa9185969692cf3d142a95b666c96c6a5c
LukasLohoff <l_loho01@uni-muenster.de>
2017-04-13 11:55:01 +0200

    add zenodo content script

---
+++ O2R Badge Integrator/Zenodo.js
@@ -0,0 +1,44 @@
+addButton();
+
+function addButton() {
+  
+  var btn = document.createElement('BUTTON');
+  var t = document.createTextNode(' Open as Executable Research Compendium');
+  var frm = document.createElement('FORM');
+  var icn = document.createElement('I');
+
+
+  icn.setAttribute('class', "fa fa-external-link");
+  btn.setAttribute('class', "btn btn-primary btn-block");
+  btn.appendChild(icn);
+  btn.appendChild(t);
+  frm.appendChild(btn);
+  //frm.setAttribute('action', "http://www.o2r.info");
+  
+  frm.addEventListener("click", function(event) {
+    
+    var collapse = document.getElementById('collapseTwo');
+    var frwrap = collapse.getElementsByClassName('forcewrap')[0];
+    var filename = frwrap.innerText;
+    
+    var zenodoURL = window.location.href;
+    var finalURL = "https://o2r.uni-muenster.de/#!/home?zenodoURL=" + zenodoURL + "&filename=" + filename;
+
+    openO2RPage(finalURL);
+    event.preventDefault();
+  });
+
+  var menu = document.getElementsByClassName("col-sm-4 col-md-4 col-right")[0];
+  var metadata = document.getElementsByClassName("well metadata")[0];
+  menu.insertBefore(frm, metadata);
+
+}
+
+function openO2RPage(url) {
+  //window.open uses default browser behavior (-> new tab) but is considered a popup and may be blocked
+  window.open(url);
+  //window.location.href = url;
+}
+
+// wait for async elements to load
+//setTimeout(addButton, 1000);
