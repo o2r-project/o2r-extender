ea23442a5f7daf3377ab73629285c859045728f9
LukasLohoff <l_loho01@uni-muenster.de>
2017-04-13 16:41:23 +0200

    change button to match badge style

--- O2R Badge Integrator/Zenodo.js
+++ O2R Badge Integrator/Zenodo.js
@@ -1,44 +1,96 @@
 addButton();
 
+var img;
+
 function addButton() {
-  
-  var btn = document.createElement('BUTTON');
-  var t = document.createTextNode(' Open as Executable Research Compendium');
-  var frm = document.createElement('FORM');
-  var icn = document.createElement('I');
 
+    var entry = document.createElement('DT');
+    var txt = document.createElement('DD');
+    var span = document.createElement('SPAN');
+    img = document.createElement('IMG');
 
-  icn.setAttribute('class', "fa fa-external-link");
-  btn.setAttribute('class', "btn btn-primary btn-block");
-  btn.appendChild(icn);
-  btn.appendChild(t);
-  frm.appendChild(btn);
-  //frm.setAttribute('action', "http://www.o2r.info");
+    entry.innerText = 'ERC:';
+    txt.setAttribute('itemprop', 'ERC');
+    span.classname = 'get-badge';
+    span.setAttribute('data-toogle', 'tooltip');
+    span.setAttribute('data-placement', 'bottom');
+    span.setAttribute('title', '');
+    span.setAttribute('data-original-title', 'Open as ERC on o2r.uni-muenster.de');
+
+    img.setAttribute('data-toogle','modal');
+    img.setAttribute('src','https://img.shields.io/badge/ERC-not available-lightgrey.svg');
+
+    setBadge();
+
+    var metadata = document.getElementsByClassName('well metadata')[0];
+    list = metadata.getElementsByTagName("DL")[0];
+
+    span.appendChild(img);
+    txt.appendChild(span);
+    list.appendChild(entry);
+    list.appendChild(txt);
   
-  frm.addEventListener("click", function(event) {
-    
+ 
+      
+    // get filename from html
     var collapse = document.getElementById('collapseTwo');
     var frwrap = collapse.getElementsByClassName('forcewrap')[0];
     var filename = frwrap.innerText;
-    
+
     var zenodoURL = window.location.href;
     var finalURL = "https://o2r.uni-muenster.de/#!/home?shareURL=" + zenodoURL + "&filename=" + filename;
 
-    openO2RPage(finalURL);
-    event.preventDefault();
-  });
-
-  var menu = document.getElementsByClassName("col-sm-4 col-md-4 col-right")[0];
-  var metadata = document.getElementsByClassName("well metadata")[0];
-  menu.insertBefore(frm, metadata);
-
+    img.setAttribute('style', 'cursor: pointer');
+    img.onclick = function() {
+        window.open(finalURL);
+    };
 }
 
 function openO2RPage(url) {
-  //window.open uses default browser behavior (-> new tab) but is considered a popup and may be blocked
-  window.open(url);
-  //window.location.href = url;
+    //window.open uses default browser behavior (-> new tab) but is considered a popup and may be blocked
+    window.open(url);
+    //window.location.href = url;
 }
 
-// wait for async elements to load
-//setTimeout(addButton, 1000);
+function setBadge() {
+    // get filename from html
+    var collapse = document.getElementById('collapseTwo');
+    // get first file
+    var frwrap = collapse.getElementsByClassName('forcewrap')[0];
+    var filename = frwrap.innerText;
+
+    if (!filename.endsWith('.zip')) {
+        return;
+    }
+
+    var splitURL = window.location.href.split('/');
+    var recordID = splitURL[splitURL.length - 1];
+
+    var requestURL = 'https://sandbox.zenodo.org/record/' + recordID + '/preview/' + filename;
+
+    var xhr = new XMLHttpRequest();
+
+    // inspect if the zip file has a folder 'data' with a file 'bagtainer.yml'
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState == XMLHttpRequest.DONE) {
+            var el = document.createElement('html');
+            el.innerHTML = xhr.responseText;
+
+            var files = el.getElementsByClassName('fa fa-file-o');
+
+            for (var i = 0; i < files.length; i++) {
+                let file = files[i];
+                if (file.parentNode.innerText.endsWith('bagtainer.yml')) {
+                    if (file.parentNode.parentNode.parentNode.id.startsWith('tree_item')) {
+                        img.setAttribute('src','https://img.shields.io/badge/ERC-inspect-0D7EBE.svg');
+                    }
+                }
+            }
+        }
+    };
+    xhr.open('GET', requestURL, true);
+    xhr.setRequestHeader('accept', '*/*');
+    xhr.send(null);
+
+
+}
