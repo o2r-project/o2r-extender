4acfbb4a12f0024a947d0607d02f8658a68ed292
LukasLohoff <lukas.lohoff@googlemail.com>
2017-05-23 22:04:31 +0200

    remove leading 'doi:' from requests

--- O2R Badge Integrator/BadgeTypes.js
+++ O2R Badge Integrator/BadgeTypes.js
@@ -2,23 +2,23 @@ var ExtendedView = false;
 // TODO: When adding a new badge, apply this here
 var BadgeTypes = [{
 		key: 'licence', value: 'Licence',
-		apiPath: 'licence/o2r/doi:', doiEncoded: true, extended: true,
+		apiPath: 'licence/o2r/', doiEncoded: true, extended: true,
 		filter: {type: 'select', values: ['open', 'mostly open', 'partially open', 'closed']}
 	},{
 		key: 'executable', value: 'Executable',
-		apiPath: 'executable/o2r/doi:', doiEncoded: true, extended: true,
+		apiPath: 'executable/o2r/:', doiEncoded: true, extended: true,
 		filter: {type: 'select', values: ['yes', 'running', 'no']}
 	},{
 		key: 'spatial', value: 'Research location',
-		apiPath: 'spatial/o2r/doi:', doiEncoded: true, extended: "iframe",
+		apiPath: 'spatial/o2r/', doiEncoded: true, extended: "iframe",
 		filter: {type: 'text'}
 	},{
 		key: 'releasetime', value: 'Release time',
-		apiPath: 'releasetime/crossref/doi:', doiEncoded: true, extended: false,
+		apiPath: 'releasetime/crossref/', doiEncoded: true, extended: false,
 		filter: {type: 'year_newer'}
 	},{
 		key: 'peerreview', value: 'Peer review',
-		apiPath: 'peerreview/doaj/doi:', doiEncoded: false, extended: false,
+		apiPath: 'peerreview/doaj/', doiEncoded: false, extended: false,
 		filter: {type: 'text'}
 	}
 ];
