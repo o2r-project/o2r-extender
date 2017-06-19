124671c711840422a4d3f67530f3d7b8e742513e
LukasLohoff <lukas.lohoff@googlemail.com>
2017-06-19 21:20:23 +0200

    fix a bug with non-encoded DOIs

--- o2r Badge Integrator/BadgeTypes.js
+++ o2r Badge Integrator/BadgeTypes.js
@@ -2,19 +2,19 @@ var ExtendedView = false;
 // TODO: When adding a new badge, apply this here
 var BadgeTypes = [{
 		key: 'licence', value: 'Licence',
-		apiPath: 'licence/', doiEncoded: true, extended: true,
+		apiPath: 'licence/', doiEncoded: false, extended: true,
 		filter: {type: 'select', values: ['open', 'mostly open', 'partially open', 'closed']}
 	},{
 		key: 'executable', value: 'Executable',
-		apiPath: 'executable/', doiEncoded: true, extended: true,
+		apiPath: 'executable/', doiEncoded: false, extended: true,
 		filter: {type: 'select', values: ['yes', 'running', 'no']}
 	},{
 		key: 'spatial', value: 'Research location',
-		apiPath: 'spatial/', doiEncoded: true, extended: "iframe",
+		apiPath: 'spatial/', doiEncoded: false, extended: "iframe",
 		filter: {type: 'text'}
 	},{
 		key: 'releasetime', value: 'Release time',
-		apiPath: 'releasetime/', doiEncoded: true, extended: false,
+		apiPath: 'releasetime/', doiEncoded: false, extended: false,
 		filter: {type: 'year_newer'}
 	},{
 		key: 'peerreview', value: 'Peer review',
